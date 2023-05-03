8aac3487932958d7bbe718768e82c41caf2051f5
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-03 02:57:16 -0600

    video 257 - data types
    section-24

--- learning-PHP/PHP-introduction/03-tipos-dato.php
+++ learning-PHP/PHP-introduction/03-tipos-dato.php
@@ -1,6 +1,23 @@
 <?php include 'includes/header.php';
 
+//booleanos
+$logueado = false;
+var_dump($logueado);
 
+//enteros
+$num = 200;
+var_dump($num);
 
+//flotantes
+$float = 200.5;
+var_dump($float);
+
+//strings
+$name = 'Juan';
+var_dump($name);
+
+//arrays
+$array = [];
+var_dump($array);
 
 include 'includes/footer.php';
