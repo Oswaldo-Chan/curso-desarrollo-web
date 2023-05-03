f6994f28abcf55633294d9b1aaa29fc897f31148
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-03 02:52:45 -0600

    video 256 - variables
    section-24

--- learning-PHP/PHP-introduction/02-variables.php
+++ learning-PHP/PHP-introduction/02-variables.php
@@ -1,6 +1,19 @@
 <?php include 'includes/header.php';
 
+$nombre = "Oswaldo";
 
+$nombre = "Oswaldo Chan";
 
+echo $nombre;
+var_dump($nombre);
+
+//constantes
+define('constante',"esto es una constante");
+
+echo constante;
+
+const constante_2 = "hola"; //no tan común
+
+echo constante_2;
 
 include 'includes/footer.php';
