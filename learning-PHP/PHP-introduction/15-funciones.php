f8198850f32d1b69a59eb98de22a647cfc6155d6
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-05 03:35:21 -0600

    video 270 - functions
    section-24

--- learning-PHP/PHP-introduction/15-funciones.php
+++ learning-PHP/PHP-introduction/15-funciones.php
@@ -1,6 +1,16 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);
+include 'includes/header.php';
 
+//funciones 
 
+function sumar(int $a = 0, float $b = 0){
+    echo $a+$b;
+}
+
+sumar(2,4.5);
+sumar(240,60.2);
+sumar(25,0.1);
 
 
 
