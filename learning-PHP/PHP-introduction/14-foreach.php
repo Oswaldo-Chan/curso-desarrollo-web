d0bfc8bb483cd3cd83653e948d5d5bdbcdb32d16
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-05 03:29:10 -0600

    video 269 - foreach
    section-24

--- learning-PHP/PHP-introduction/14-foreach.php
+++ learning-PHP/PHP-introduction/14-foreach.php
@@ -1,6 +1,33 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'name' => 'tablet',
+        'costo' => 200,
+        'disponible' => true
+    ],
+    [
+        'name' => 'celular',
+        'costo' => 100,
+        'disponible' => false
+    ],
+    [
+        'name' => 'monitor',
+        'costo' => 550,
+        'disponible' => true
+    ]
+];
 
+foreach ($productos as $producto) { ?> <!-- php termina aqui -->
 
+    <li>
+        <p>Producto: <?php echo $producto['name']; ?></p>
+        <p>Precio: <?php echo "$".$producto['costo']; ?></p>
+        <!-- Operador Ternario -->
+        <p>Precio: <?php echo ($producto['disponible']) ? 'disponible' : 'no disponible' ?></p>
+    </li>
+
+    <?php
+}
 
 include 'includes/footer.php';
