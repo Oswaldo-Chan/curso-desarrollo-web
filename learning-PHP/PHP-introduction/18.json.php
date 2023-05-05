c7253c12f3fc0475263ac687d0bda8c9dd5c5f7d
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-05 04:03:20 -0600

    video 274 - json_encode and json_decode (SECTION 24 COMPLETED)
    section-24 completed

--- learning-PHP/PHP-introduction/18.json.php
+++ learning-PHP/PHP-introduction/18.json.php
@@ -1,8 +1,29 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'name' => 'tablet',
+        'costo' => 200,
+        'disponible' => true
+    ],
+    [
+        'name' => 'celular',
+        'costo' => 100,
+        'disponible' => false
+    ],
+    [
+        'name' => 'monitor',
+        'costo' => 550,
+        'disponible' => true
+    ]
+];
 
-
-
-
+echo "<pre>";
+var_dump($productos);
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE); //string con json
+$json_array = json_decode($json); //de string a arreglo
+var_dump($json);
+var_dump($json_array);
+echo "</pre>";
 
 include 'includes/footer.php';
