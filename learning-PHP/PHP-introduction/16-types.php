ea6c68abb2842c4e1256167f5725615815a1b48b
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-05 03:42:53 -0600

    video 271 - functions that return a value and data types
    section-24

--- learning-PHP/PHP-introduction/16-types.php
+++ learning-PHP/PHP-introduction/16-types.php
@@ -1,7 +1,26 @@
 <?php include 'includes/header.php';
 
+function userAuth(bool $auth) : string { //debe retornar un string
+    if ($auth) {
+        return "Usuario Autenticado";
+    } else {
+        return "Usuario No Autenticado";
+    }
+}
 
+function imprimir($dato) : void {
+    echo $dato;
+}
 
+$user = userAuth(true);
+imprimir($user);
 
+function funcion(bool $b) :string|int {
+    if($b){
+        return "hola";
+    } else {
+        return true;
+    }
+}
 
 include 'includes/footer.php';
