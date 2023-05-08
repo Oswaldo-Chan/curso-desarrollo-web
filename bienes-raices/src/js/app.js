3e62a8f498174599cce44b398332aa7f31c470ce
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-08 03:33:05 -0600

    video 300 - added dark mode
    section-27

--- bienes-raices/src/js/app.js
+++ bienes-raices/src/js/app.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     eventListeners();
+    darkMode();
 });
+
+function darkMode() {
+    const btnDarkMode = document.querySelector('.btn-dark-mode');
+
+    btnDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+    });
+}
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
