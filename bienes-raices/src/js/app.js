7f368f9f567e245481eea384b1c11d805ff91a0c
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-08 03:55:17 -0600

    video 302 - added native dark mode
    section-27 completed

--- bienes-raices/src/js/app.js
+++ bienes-raices/src/js/app.js
@@ -5,7 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function darkMode() {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
     const btnDarkMode = document.querySelector('.btn-dark-mode');
+
+    if (prefersDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+
+    }
+
+    prefersDarkMode.addEventListener('change', function() {
+        if (prefersDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+    
+        }
+    });
 
     btnDarkMode.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
