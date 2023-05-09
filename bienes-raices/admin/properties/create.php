29d85e3ed6ca0b19d1ee4ece6d2f556746d3fb4b
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-08 21:19:13 -0600

    video 311 - added form to create properties
    section-30

--- bienes-raices/admin/properties/create.php
+++ bienes-raices/admin/properties/create.php
@@ -7,6 +7,47 @@
         <h1>Crear</h1>
 
         <a href="/admin" class="btn btn-purple">Regresar</a>
+
+        <form class="form">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="title">Titulo:</label>
+                <input type="text" id="title" placeholder="Titulo de la propiedad">
+
+                <label for="price">Precio:</label>
+                <input type="number" id="price" placeholder="precio de la propiedad">
+
+                <label for="image">Imagen:</label>
+                <input type="file" id="image" accept="image/jpeg, image/png">
+
+                <label for="description">Descripcion:</label>
+                <textarea id="description"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información de la Propiedad</legend>
+
+                <label for="rooms">Habitaciones:</label>
+                <input type="number" id="rooms" placeholder="Ej: 5" min="1" max="10">
+            
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 5" min="1" max="10">
+
+                <label for="parking">Estacionamiento:</label>
+                <input type="number" id="parking" placeholder="Ej: 5" min="1" max="10">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select>
+                    <option value="1">Oswaldo</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="btn btn-yellow">
+        </form>
     </main>
 
 <?php
