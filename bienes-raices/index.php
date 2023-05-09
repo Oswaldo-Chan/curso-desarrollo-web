d123c1f4d294a2aac173f5df4d173aa29f7edfcb
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-08 18:56:23 -0600

    video 307 - modified footer
    section-29

--- bienes-raices/index.php
+++ bienes-raices/index.php
@@ -189,19 +189,6 @@
         </section>
     </div>
     
-    <footer class="footer section">
-        <div class="container footer-content">
-            <nav class="nav">
-                <a href="nosotros.html">nosotros</a>
-                <a href="anuncios.html">anuncios</a>
-                <a href="blog.html">blog</a>
-                <a href="contacto.html">contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">todos los derechos reservados 2023 &copy;</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php
+    include './includes/templates/footer.php';
+?>
