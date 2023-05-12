d4ae65330e65af22dc3132319fb7f5629bcb07e1
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-12 04:00:25 -0600

    video 234 - added a properties table
    section-30

--- bienes-raices/admin/index.php
+++ bienes-raices/admin/index.php
@@ -11,6 +11,30 @@
         <?php endif; ?>
     
         <a href="/admin/properties/create.php" class="btn btn-purple">Nueva Propiedad</a>
+    
+            <table class="table-properties">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>titulo</th>
+                        <th>imagen</th>
+                        <th>precio</th>
+                        <th>acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Casa en la playa</td>
+                        <td> <img src="/img/071a629a3c58143ef6c0d60dee602e60.jpg" class="table-image"></td>
+                        <td>1200000</td>
+                        <td>
+                            <a class="btn-red-block" href="#">Eliminar</a>
+                            <a class="btn-green-block" href="#">Actualizar</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
     </main>
 
 <?php
