85e08693e89b5ba6d4ea52cc9d3f439b0bb997b8
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-14 04:36:32 -0600

    video 335 - hashing passwords
    section-31

--- bienes-raices/user.php
+++ bienes-raices/user.php
@@ -5,8 +5,9 @@ $db = connectDB();
 //crear email y password
 $email = 'correo@correo.com';
 $password = 'qwerty123';
+$passwordHash = password_hash($password,PASSWORD_DEFAULT);
 //query para crear usuario
 $query = "INSERT INTO usuarios (email, password) ";
-$query .= "VALUES ('{$email}','{$password}')";
+$query .= "VALUES ('{$email}','{$passwordHash}')";
 //agregarlo a la base de datos
 mysqli_query($db,$query);
