9125b278a2b7a2d8f245a0d9cc37f1a9b52f38e0
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-15 03:57:11 -0600

    video 346 - class attributes in PHP8
    section-32

--- learning-PHP/PHP-POO/01.php
+++ learning-PHP/PHP-POO/01.php
@@ -3,15 +3,8 @@ declare(strict_types = 1);
 include 'includes/header.php';
 //creando una clase
 class Producto {
-    public $nombre;
-    public $precio;
-    public $disponible;
-
-    public function __construct(string $nombre, float $precio, bool $disponible) {
-        $this->nombre = $nombre;
-        $this->precio = $precio;
-        $this->disponible = $disponible;
-    }   
+    public function __construct(public string $nombre, 
+    public float $precio, public bool $disponible) {}   
 }
 
 $producto = new Producto('tablet',200,true);
@@ -19,9 +12,9 @@ $producto = new Producto('tablet',200,true);
 echo '<pre>';
 var_dump($producto);
 echo '</pre>';
+
 $producto2 = new Producto('monitor',1000,true);
 
-echo $producto2->nombre;
 echo '<pre>';
 var_dump($producto2);
 echo '</pre>';
