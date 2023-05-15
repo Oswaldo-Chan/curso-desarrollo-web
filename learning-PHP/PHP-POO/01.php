1929fada902898cb8de5ef7416ad2b74dd9ab1ae
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-15 01:56:47 -0600

    video 344 - PHP classes
    section-32

--- learning-PHP/PHP-POO/01.php
+++ learning-PHP/PHP-POO/01.php
@@ -1,5 +1,28 @@
 <?php include 'includes/header.php';
 
+//creando una clase
+class Producto {
+    public $nombre;
+    public $precio;
+    public $disponible;
 
+
+}
+
+$producto = new Producto();
+$producto->nombre = 'tablet';
+$producto->precio = 200;
+$producto->disponible = true;
+echo '<pre>';
+var_dump($producto);
+echo '</pre>';
+$producto2 = new Producto();
+$producto2->nombre = 'monitor';
+$producto2->precio = 1000;
+$producto2->disponible = false;
+echo $producto2->nombre;
+echo '<pre>';
+var_dump($producto2);
+echo '</pre>';
 
 include 'includes/footer.php';
