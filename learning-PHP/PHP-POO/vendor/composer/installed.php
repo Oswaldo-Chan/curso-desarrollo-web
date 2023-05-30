499eaf59cdf6d43350e4ae186ba9cb74d4c1cc2c
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-05-30 04:42:13 -0600

    video 356 - autoload with composer
    section-32

---
+++ learning-PHP/PHP-POO/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'joswa/php-poo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '39c865b44a68ebe54c93f942849c6ab5c593bfc7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.5.0',
+            'version' => '6.5.0.0',
+            'reference' => 'e94e7353302b0c11ec3cfff7180cd0b1743975d2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'joswa/php-poo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '39c865b44a68ebe54c93f942849c6ab5c593bfc7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
