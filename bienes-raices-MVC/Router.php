33bfbf64334670d5196f51c5450c3191aa750eac
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-05 23:20:23 -0600

    video 398 - associating methods with routes
    section-41

--- bienes-raices-MVC/Router.php
+++ bienes-raices-MVC/Router.php
@@ -3,7 +3,26 @@
 namespace MVC; 
 
 class Router {
-    public function __construct() {
-        echo "qwerty";
+
+    public $routesGET = [];
+    public $routesPOST = [];
+
+    public function get($url, $function) {
+        $this->routesGET[$url] = $function;
+    }
+    
+    public function checkRoutes() {
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $method = $_SERVER['REQUEST_METHOD']; 
+
+        if($method === 'GET') {
+            $function = $this->routesGET[$currentUrl] ?? null;
+        }
+
+        if($function) {
+            call_user_func($function, $this);
+        } else {
+            echo "Pagina No Encontrada";
+        }
     }
 }
--- bienes-raices-MVC/public/index.php
+++ bienes-raices-MVC/public/index.php
@@ -5,3 +5,9 @@ require_once __DIR__.'/../includes/app.php';
 use MVC\Router;
 
 $router = new Router();
+
+$router->get('/nosotros', 'f_nosotros');
+$router->get('/admin', 'f_admin');
+$router->get('/propiedades', 'f_propiedades');
+
+$router->checkRoutes();
