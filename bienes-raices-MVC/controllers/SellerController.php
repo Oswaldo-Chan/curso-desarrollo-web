b8a4e3f58f89c4931180b2314869e00b7ef7f0b7
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-07 02:53:53 -0600

    video 411 - added a sellers controller
    section-41

---
+++ bienes-raices-MVC/controllers/SellerController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controllers;
+
+class SellerController {
+    public static function create() {
+
+    }
+    public static function update() {
+
+    }
+    public static function delete() {
+
+    }
+}
--- bienes-raices-MVC/public/index.php
+++ bienes-raices-MVC/public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../includes/app.php';
 
 use MVC\Router;
 use Controllers\PropertyController;
+use Controllers\SellerController;
 
 $router = new Router();
 
@@ -13,5 +14,11 @@ $router->post('/properties/create', [PropertyController::class, 'create']);
 $router->get('/properties/update', [PropertyController::class, 'update']);
 $router->post('/properties/update', [PropertyController::class, 'update']);
 $router->post('/properties/delete', [PropertyController::class, 'delete']);
+
+$router->get('/sellers/create', [SellerController::class, 'create']);
+$router->post('/sellers/create', [SellerController::class, 'create']);
+$router->get('/sellers/update', [SellerController::class, 'update']);
+$router->post('/sellers/update', [SellerController::class, 'update']);
+$router->post('/sellers/delete', [SellerController::class, 'delete']);
 
 $router->checkRoutes();
