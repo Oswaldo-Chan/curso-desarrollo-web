ecd6ebbed9d372cbfc1e41a0049f1c1008f0942a
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-09 01:40:09 -0600

    video 431 - modified admin class
    section-43

--- bienes-raices-MVC/models/Admin.php
+++ bienes-raices-MVC/models/Admin.php
@@ -3,5 +3,38 @@
 namespace Model;
 
 class Admin extends ActiveRecord {
+    protected static $table = 'usuarios';
+    protected static $dbColumns = ['id','nombre','apellido','email','password'];
     
+    public $id;
+    public $name;
+    public $surname;
+    public $email;
+    public $password;
+
+    public function __construct($args = [])
+    {  
+        $this->id = $args['id'] ?? null;
+        $this->name = $args['name'] ?? '';
+        $this->surname = $args['surname'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password  = $args['password'] ?? '';
+    
+    }
+    protected static function getPropertyForColumn($column) {
+        switch ($column) {
+            case 'id':
+                return 'id';
+            case 'nombre':
+                return 'name';
+            case 'apellido':
+                return 'surname';
+            case 'email':
+                return 'email';
+            case 'password':
+                return 'password';
+            default:
+                return null;
+        }
+    }
 }
