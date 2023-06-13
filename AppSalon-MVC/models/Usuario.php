c06ae389b8b6ec5fcb393bf53535d75a00bc7e93
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-13 04:54:48 -0600

    video 461 - added User class
    section-48 started

---
+++ AppSalon-MVC/models/Usuario.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Model;
+
+class Usuario extends ActiveRecord {
+    protected static $table = 'usuarios';
+    protected static $columnasDB = ['id','nombre','apellido','email','telefono','admin','confirmado','token'];
+    public $id;
+    public $nombre;
+    public $apellido;
+    public $email;
+    public $password;
+    public $telefono;
+    public $admin;
+    public $confirmado;
+    public $token;
+
+    public function __construct($args = []) {
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->apellido = $args['apellido'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+        $this->telefono = $args['telefono'] ?? '';
+        $this->admin = $args['admin'] ?? null;
+        $this->confirmado = $args['confirmado'] ?? null;
+        $this->token = $args['token'] ?? '';
+    }
+}
