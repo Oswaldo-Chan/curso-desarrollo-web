681a90be29f1e60f472aed6de5cb410d3f6b36bc
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-18 23:57:16 -0600

    video 500 - displaying services
    section-53

--- AppSalon-MVC/src/js/app.js
+++ AppSalon-MVC/src/js/app.js
@@ -86,8 +86,31 @@ async function consultarAPI() {
         const url = '/api/servicios';
         const resultado = await fetch(url);
         const servicios = await resultado.json();
-        console.log(servicios);
+        mostrarServicios(servicios);
     } catch (error) {
         console.log(error);
     }
+}
+
+function mostrarServicios(servicios) {
+    servicios.forEach( servicio => {
+        const {id, nombre, precio} = servicio;
+
+        const nombreServicio = document.createElement('P');
+        nombreServicio.classList.add('nombre-servicio');
+        nombreServicio.textContent = nombre;
+
+        const precioServicio = document.createElement('P');
+        precioServicio.classList.add('precio-servicio');
+        precioServicio.textContent = `$ ${precio}`;
+
+        const servicioDiv = document.createElement('DIV');
+        servicioDiv.classList.add('servicio');
+        servicioDiv.dataset.idServicio = id;
+
+        servicioDiv.appendChild(nombreServicio);
+        servicioDiv.appendChild(precioServicio);
+
+        document.querySelector('#servicios').appendChild(servicioDiv);
+    });
 }
