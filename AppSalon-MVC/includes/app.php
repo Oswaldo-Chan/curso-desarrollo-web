88ed7c5877f31040408a2fc3a936496aecc8587e
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-19 21:25:41 -0600

    modified dotenv file & database.php
    modified appsalon project

--- AppSalon-MVC/includes/app.php
+++ AppSalon-MVC/includes/app.php
@@ -1,8 +1,9 @@
 <?php 
-
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->safeLoad();
+
 require 'funciones.php';
 require 'database.php';
 
--- AppSalon-MVC/includes/database.php
+++ AppSalon-MVC/includes/database.php
@@ -1,6 +1,7 @@
 <?php
 
 $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
+$db->set_charset("utf8");
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
