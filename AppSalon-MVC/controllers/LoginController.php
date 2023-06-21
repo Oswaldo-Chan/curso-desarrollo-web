3f02e11c1c4634b57bc66155c5cf7edec0f4f5fd
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-20 23:08:10 -0600

    video 529 - added logout button
    section-55 completed

--- AppSalon-MVC/controllers/LoginController.php
+++ AppSalon-MVC/controllers/LoginController.php
@@ -48,7 +48,13 @@ class LoginController {
         ]);
     }
     public static function logout() {
-        echo "Desde Logout";
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $_SESSION = [];
+
+        header('Location: /');
     }
     public static function passwordOlvidado(Router $router) {
         $alertas = [];
--- AppSalon-MVC/views/cita/index.php
+++ AppSalon-MVC/views/cita/index.php
@@ -3,6 +3,7 @@
 
 <div class="barra">
     <p>Hola, <?php echo $nombre ?? '' ?></p>
+    <a class="btn" href="/logout">Cerrar Sesión</a>
 </div>
 
 <div class="app">
