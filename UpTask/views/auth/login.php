ef4bfd7eb92bd483e2bc71afcd9cb07a538a5941
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-22 04:30:59 -0600

    video 566 - modified login view
    section-61

--- UpTask/views/auth/login.php
+++ UpTask/views/auth/login.php
@@ -1,1 +1,26 @@
-<h1>Login</h1>
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crea y Administra tus Proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+        <form action="/" method="POST" class="formulario">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Ingrese su email">
+            </div>
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Ingrese su password">
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">¿Aún no tienes una cuenta? obtén una</a>
+            <a href="/olvide">¿Olvidaste tu password? restablecer password</a>
+        </div>
+    </div> <!-- contenedor sm -->
+</div>
