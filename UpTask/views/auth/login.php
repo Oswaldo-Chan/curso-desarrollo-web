b89246b86dbb549ceea34ef55ffaa6f87c18bb26
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-26 02:27:09 -0600

    video 567 - added styles to login form
    section-61

--- UpTask/views/auth/login.php
+++ UpTask/views/auth/login.php
@@ -1,6 +1,6 @@
 <div class="contenedor">
     <h1>UpTask</h1>
-    <p>Crea y Administra tus Proyectos</p>
+    <p class="tagline">Crea y Administra tus Proyectos</p>
 
     <div class="contenedor-sm">
         <p class="descripcion-pagina">Iniciar Sesión</p>
