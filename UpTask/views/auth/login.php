f548262df112bc4f6fed3060baa1d544c77b34f0
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-26 02:48:49 -0600

    video 569,570 - finished styles to login page
    section-61 completed

--- UpTask/views/auth/login.php
+++ UpTask/views/auth/login.php
@@ -1,5 +1,5 @@
-<div class="contenedor">
-    <h1>UpTask</h1>
+<div class="contenedor login">
+    <h1 class="uptask">UpTask</h1>
     <p class="tagline">Crea y Administra tus Proyectos</p>
 
     <div class="contenedor-sm">
