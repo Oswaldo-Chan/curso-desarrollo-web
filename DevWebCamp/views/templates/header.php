f362b5072702357bcaf9f1941fd71e71cf880e0e
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-07-02 04:08:43 -0600

    video 679,680,681,682,683,684 - added header
    section-79 completed

---
+++ DevWebCamp/views/templates/header.php
@@ -0,0 +1,32 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/login" class="header__enlace">Iniciar Sesión</a>
+        </nav>
+
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">&#60; DevWebCamp /></h1>
+            </a>
+
+            <p class="header__texto">Octubre 5-6 - 2023</p>
+            <p class="header__texto header__texto--modalidad">En Línea - Presencial</p>
+
+            <a href="/registro" class="header__boton">Comprar Pase</a>
+        </div>
+    </div>
+</header>
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/">
+            <h2 class="barra__logo">&#60; DevWebCamp /></h2>
+        </a>
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshops-conferencias" class="navegacion__enlace">Workshops / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
