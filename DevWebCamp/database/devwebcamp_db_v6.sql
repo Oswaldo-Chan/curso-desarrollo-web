21e1a7f101b680a8610ba90ffd2b22c6573c9ce3
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-07-10 05:29:38 -0600

    updated database file
    devwebcamp project

--- DevWebCamp/database/devwebcamp_db_v6.sql
+++ DevWebCamp/database/devwebcamp_db_v6.sql
@@ -126,6 +126,30 @@ INSERT INTO `horas` VALUES (1,'10:00 - 10:55'),(2,'11:00 - 11:55'),(3,'12:00 - 1
 UNLOCK TABLES;
 
 --
+-- Table structure for table `paquetes`
+--
+
+DROP TABLE IF EXISTS `paquetes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paquetes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paquetes`
+--
+
+LOCK TABLES `paquetes` WRITE;
+/*!40000 ALTER TABLE `paquetes` DISABLE KEYS */;
+INSERT INTO `paquetes` VALUES (1,'Presencial'),(2,'Virtual'),(3,'Gratuito');
+/*!40000 ALTER TABLE `paquetes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `ponentes`
 --
 
@@ -153,6 +177,37 @@ LOCK TABLES `ponentes` WRITE;
 /*!40000 ALTER TABLE `ponentes` DISABLE KEYS */;
 INSERT INTO `ponentes` VALUES (1,' Julian','Muñoz','Madrid','España','6764a74ccf2b4b5b74e333016c13388a','React,PHP,Laravel','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"\",\"github\":\"https://github.com/codigoconjuan\"}'),(2,' Israel','González','CDMX','México','6497c66bcc464e26871c046dd5bb86c8','Vue,Node.js,MongoDB','{\"facebook\":\"\",\"twitter\":\"\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"https://tiktok.com/@codigoconjuan\",\"github\":\"https://github.com/codigoconjuan\"}'),(3,' Isabella','Tassis','Buenos Aires','Argentina','55c7866df31370ec3299eed6eb63daa1','UX / UI,HTML,CSS,TailwindCSS','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"\",\"tiktok\":\"https://tiktok.com/@codigoconjuan\",\"github\":\"\"}'),(4,' Jazmín','Hurtado','CDMX','México','de6e3fa0cde8402de4c28e6be0903ada','Django,React, Vue.js','{\"facebook\":\"\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"\",\"github\":\"https://github.com/codigoconjuan\"}'),(5,' María Camila','Murillo','Guadalajara','México','cec8c9d7bcb4c19e87d1164bce8fe176','Devops,Git,CI CD','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"\",\"tiktok\":\"\",\"github\":\"\"}'),(6,' Tomas','Aleman','Bogotá','Colombia','5332118b8d7690a1b992431802eafab1','WordPress,PHP,React','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"\",\"github\":\"https://github.com/codigoconjuan\"}'),(7,' Lucía','Velázquez','Buenos Aires','Argentina','90956ece4adbd9f9ccb8f4ae80dc1537','React,Angular,Svelte','{\"facebook\":\"\",\"twitter\":\"\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"https://tiktok.com/@codigoconjuan\",\"github\":\"https://github.com/codigoconjuan\"}'),(8,' Catarina','Pardo','Lima','Perú','9886ffc0d31e4c20a04acc1464630527','Next.js,Laravel,MERN','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"\",\"tiktok\":\"https://tiktok.com/@codigoconjuan\",\"github\":\"\"}'),(9,' Raquel','Ros','Madrid','España','d697f6c454c36252d70abacd7599566c','Next.js,Remix,Vue.js','{\"facebook\":\"\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"\",\"github\":\"https://github.com/codigoconjuan\"}'),(10,' Sofía','Amengual','Santiago','Chile','414ffd61380bbf0e9f45cbde4d0cbb7f','UX / UI,Figma,TailwindCSS','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"\",\"tiktok\":\"\",\"github\":\"\"}'),(11,' María José','Leoz','NY','Estados Unidos','c8b3a77bce7a6efb6e6872db67cfa553','React,TypeScript,JavaScript','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"\",\"github\":\"https://github.com/codigoconjuan\"}'),(12,' Alexa','Mina','Bogotá','Colombia','6eac63d88743bbb9ee0bfd8c60cf4186','HTML,CSS,React,TailwindCSS','{\"facebook\":\"\",\"twitter\":\"\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"https://tiktok.com/@codigoconjuan\",\"github\":\"https://github.com/codigoconjuan\"}'),(13,' Jesus','López','Madrid','España','e481bca0c512f5b4d8f76ccea2548f0d','PHP,WordPress,HTML,CSS','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"\",\"tiktok\":\"https://tiktok.com/@codigoconjuan\",\"github\":\"\"}'),(14,' Irene ','Dávalos','CDMX','México','6727e8ee7f6c642026247fe0556d866d','Node.js,Vue.js,Angular','{\"facebook\":\"\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"\",\"github\":\"https://github.com/codigoconjuan\"}'),(15,' Brenda',' Ocampo','Santiago','Chile','40e01f5c023c7e74c9c372a8126edd97','Laravel,Next.js,GraphQL,Flutter','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"\",\"tiktok\":\"\",\"github\":\"\"}'),(16,' Julián ','Noboa','Las Vegas','EU','6d4629dacbed2e4f5a344282ec2f4f76','iOS,Figma,REST API\'s','{\"facebook\":\"\",\"twitter\":\"\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"https://instagram.com/codigoconjuan\",\"tiktok\":\"https://tiktok.com/@codigoconjuan\",\"github\":\"https://github.com/codigoconjuan\"}'),(17,' Vicente ','Figueroa','CDMX','México','2a41a781d8ae8f0f7a1969c766276b08','React,Tailwind,JavaScript,TypeScript,Node','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"\",\"tiktok\":\"\",\"github\":\"\"}'),(18,'Nico','Fraga','Buenos Aires','Argentina','222dc6502643afa2f4a55acaecd93221','PHP,Laravel,Flutter,React Native','{\"facebook\":\"https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628\",\"twitter\":\"https://twitter.com/codigoconjuan\",\"youtube\":\"https://youtube.com/codigoconjuan\",\"instagram\":\"\",\"tiktok\":\"\",\"github\":\"\"}');
 /*!40000 ALTER TABLE `ponentes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registros`
+--
+
+DROP TABLE IF EXISTS `registros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `registros` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `paquete_id` int DEFAULT NULL,
+  `pago_id` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `token` varchar(8) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `usuario_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuarioId` (`usuario_id`),
+  KEY `paquete_id` (`paquete_id`),
+  CONSTRAINT `registros_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `registros_ibfk_2` FOREIGN KEY (`paquete_id`) REFERENCES `paquetes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registros`
+--
+
+LOCK TABLES `registros` WRITE;
+/*!40000 ALTER TABLE `registros` DISABLE KEYS */;
+INSERT INTO `registros` VALUES (1,1,'9XN51843SP468392M','c7700517',11);
+/*!40000 ALTER TABLE `registros` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -194,4 +249,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-07  4:49:40
+-- Dump completed on 2023-07-10  5:28:59
